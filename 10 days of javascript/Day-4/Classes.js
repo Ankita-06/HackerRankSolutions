6fb5b5aa501dd4a80653bd2e0f05f000c97c6573
Ankita-06 <74970295+Ankita-06@users.noreply.github.com>
2021-04-12 10:48:01 +0530

    Update Classes.js

--- 10 days of javascript/Day-4/Classes.js
+++ 10 days of javascript/Day-4/Classes.js
@@ -6,9 +6,17 @@ class Polygon{
         return this.side.reduce(function add(a,b){return a+b;})
     } 
 }
-    /*
+/*
  * Implement a Polygon class with the following properties:
  * 1. A constructor that takes an array of integer side lengths.
  * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
  */
 
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
