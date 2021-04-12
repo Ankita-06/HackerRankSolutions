05943da9bd5fa6df3e08c4d6ea36515d9a79584b
Ankita-06 <74970295+Ankita-06@users.noreply.github.com>
2021-04-12 10:43:45 +0530

    Update Try, Catch, and Finally.js

--- 10 days of javascript/Day-3/Try, Catch, and Finally.js
+++ 10 days of javascript/Day-3/Try, Catch, and Finally.js
@@ -1,13 +1,33 @@
+'use strict';
 
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
 
 /*
  * Complete the reverseString function
  * Use console.log() to print to stdout.
  */
 function reverseString(s) {
-    
-  
-  try{
+     try{
     // Can be chained, but it damages readability
     console.log(s.split("").reverse().join("")) 
   }
@@ -15,5 +35,12 @@ function reverseString(s) {
     console.log(e.message); // Use .message, or you'll get more than expected.
     console.log(s);
   }
+    
+}
 
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
 }
